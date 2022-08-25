942fddcf70cbc1dc399c3a2ac5394230743c1894
Antonio Alliegro <antonioalliegro1@gmail.com>
2022-08-25 12:53:09 +0200

    update download_data.sh
    refactoring + simplification of the data download procedure

--- download_data.sh
+++ download_data.sh
@@ -1,22 +1,47 @@
 #!/bin/bash
-dir=$(pwd)  # change here datasets root if needed
-mkdir -p "$dir"
-echo "Downloading data in $dir"
+data_dir=$(pwd)/3D_OS_release_data  # change here data_dir if required
+mkdir -p "$data_dir"  # make dir if it does not exists
+echo "Downloading data in ${data_dir}"
 
-# download data
-wget -O "${dir}/3D_OS_release_data.tar" "https://www.dropbox.com/s/2ta5o5sa4q69fsb/3D_OS_release_data.tar?dl=1"
-# modelnet
-echo "Downloading modelnet in $dir"
-wget --no-check-certificate -O "${dir}/modelnet40_normal_resampled.zip" "https://shapenet.cs.stanford.edu/media/modelnet40_normal_resampled.zip"
 
-# extract data
-tar -xf "${dir}/3D_OS_release_data.tar" -C "$dir"
-# move modelnet zip to 3D_OS_release_data and extract
-mv "${dir}/modelnet40_normal_resampled.zip" "${dir}/3D_OS_release_data/"
-unzip -d "${dir}/3D_OS_release_data/" "${dir}/3D_OS_release_data/modelnet40_normal_resampled.zip"
+# sncore
+printf "============"
+echo "Downloading ShapeNetCore resampled in $dir"
+sncore="https://www.dropbox.com/s/oa3qbujpugw4d43/sncore_fps_4096.tar?dl=1"
+wget -O "${data_dir}/tmp_sncore_fps_4096.tar" "$sncore"  # download
+tar -xf "${data_dir}/tmp_sncore_fps_4096.tar" -C "$data_dir"  # extract
+rm "${data_dir}/tmp_sncore_fps_4096.tar" # clean
+printf "============\n"
 
-# clean
-echo "Cleaning.."
-rm "${dir}/3D_OS_release_data/modelnet40_normal_resampled.zip"
-rm "${dir}/3D_OS_release_data.tar"
+
+# md40 + ood splits
+printf "============"
+echo "Downloading ModelNet40 + OOD Splits in $dir"
+md40="https://www.dropbox.com/s/c2x3h59nxprjs21/modelnet40_normal_resampled.tar?dl=1"
+wget -O "${data_dir}/tmp_modelnet40_normal_resampled.tar" "$md40"  # download
+tar -xf "${data_dir}/tmp_modelnet40_normal_resampled.tar" -C "$data_dir"  # extract
+rm "${data_dir}/tmp_modelnet40_normal_resampled.tar" # clean
+printf "============\n"
+
+
+# scanobject
+printf "============"
+echo "Downloading ScanObjectNN in $dir"
+sonn="https://www.dropbox.com/s/gu0p3rych1k26b7/ScanObjectNN.tar?dl=1"
+wget -O "${data_dir}/tmp_ScanObjectNN.tar" "$sonn"  # download
+tar -xf "${data_dir}/tmp_ScanObjectNN.tar" -C "$data_dir"  # extract
+rm "${data_dir}/tmp_ScanObjectNN.tar" # clean
+printf "============\n"
+
+
+# md40 corruputed
+printf "============"
+echo "Downloading ModelNet40 with corruptions in $dir"
+md40_corruptions="https://www.dropbox.com/s/qta41f1wucyktzx/ModelNet40_corrupted.tar?dl=1"
+wget -O "${data_dir}/tmp_ModelNet40_corrupted.tar" "$md40_corruptions"  # download
+tar -xf "${data_dir}/tmp_ModelNet40_corrupted.tar" -C "$data_dir"  # extract
+rm "${data_dir}/tmp_ModelNet40_corrupted.tar" # clean
+printf "============\n"
+
+
 echo "Finished"
